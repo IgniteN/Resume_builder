fccc0f68009fb686fc9e6d404845e34ec28c8ad2
Tarun-0007 <tarunchauhan495@gmail.com>
2024-07-28 09:53:53 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
-import "./App.css";
+
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Login from "./components/login";
+import Register from "./components/register";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Admin from "./components/Admin";
+import "./components/App.css";
 import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center">Let's generate your Resume!</h1>
-      <Form />
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="home" element={<Home />} />
+          <Route path="admin" element={<Admin />} />
+          <Route path="form" element={<Form />} /> {/* Add this route */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
